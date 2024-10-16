a5dad9a1816b276f988d7a29469dc881e401f6a6
nhatvu148 <nhatvu148@gmail.com>
2024-10-17 01:10:24 +0700

    add form.php

---
+++ php/form.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+
+    <!-- <form action="welcome.php" method="GET"> -->
+    <form action="welcome.php" method="POST">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>
