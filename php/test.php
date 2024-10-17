f8753c00095b4ff2cf7604ba51cc58fd4858e982
nhatvu148 <nhatvu148@gmail.com>
2024-10-17 11:15:03 +0700

    add test.php

--- php/test.php
+++ php/test.php
@@ -78,6 +78,16 @@
     var_dump($colors);
 
     echo "</br>";
+
+    $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+    echo json_encode($age);
+
+    echo "</br>";
+
+    $jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+    var_dump(json_decode($jsonobj));
+
+    echo "</br>";
 ?>
 <!DOCTYPE html>
 <html>
